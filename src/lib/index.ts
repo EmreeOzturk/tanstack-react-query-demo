7374abab5af135ec55113563e0fa23990690766a
Emre Ozturk <emreeozturk42@gmail.com>
2023-09-07 02:47:00 +0300

    codes optimized

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
 import { User } from "./../../types.d";
 
-export const API_URL = "https://jsonplaceholder.typicode.com";
-export const USER_QUERY_KEY = "users";
-export async function getUsers(): Promise<User[]> {
+const API_URL = "https://jsonplaceholder.typicode.com";
+const USER_QUERY_KEY = "users";
+
+async function getUsers(): Promise<User[]> {
   const response = await fetch(`${API_URL}/${USER_QUERY_KEY}`);
   const users = await response.json();
   return users;
 }
+
+export const useUsersQuery = () => useQuery<User[]>([USER_QUERY_KEY], getUsers);
